4e709765f553ddbbb3b90012c71520039aa5ebd5
Nikolay Nedyalkov Petkov <92088741+nikolaigod@users.noreply.github.com>
2022-07-08 17:35:14 +0300

    Add files via upload

---
+++ CS_Basics_Week1/CS_Basics_Week1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CS_Basics_Week1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double convertNum = double.Parse(Console.ReadLine());
+            double bgnNum = convertNum * 1.79549;
+            Console.WriteLine(bgnNum);
+            
+        }
+    }
+}
